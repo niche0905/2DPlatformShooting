c73d17d0a8c8640ba7e1fb47e2f336aed76e5176
niche0905 <yudle537@gmail.com>
2024-08-18 13:14:32 +0900

    save함수 구현
    Level 정보를 저장하는 save 함수를 구현하였다

--- 2D Platform Shooting/Level.cpp
+++ 2D Platform Shooting/Level.cpp
@@ -1,6 +1,31 @@
 #include "Level.h"
 
 
+bool Level::save(const std::string& filename)
+{
+	std::ofstream outFile(filename);
+
+	// 파일이 열리지 않았다면
+	if (not outFile.is_open())
+		return false;
+
+	size_t platformCount = platforms.size();
+
+	outFile << platformCount << "\n";
+	for (const auto& platform : platforms) {
+		outFile << platform << "\n";
+	}
+	
+	outFile.close();
+
+	return true;
+}
+
+bool load(const std::string& filename)
+{
+	return false;
+}
+
 void Level::draw(sf::RenderWindow& window)
 {
 	// platforms에 있는 모든 platform을 그린다
