73c11288f1ec63f1dc567cb18f8fe0b00f7d3aa3
niche0905 <yudle537@gmail.com>
2024-08-31 14:28:26 +0900

    다시 게임으로

--- 2D Platform Shooting/main.cpp
+++ 2D Platform Shooting/main.cpp
@@ -9,16 +9,8 @@
 int main()//
   //-----// Game 실행하기 위한 Main
 {
-    std::ifstream inFile(GunSavePath);
-    Gun gun1, gun2;
-    inFile >> gun1 >> gun2;
-    inFile.close();
-
-    std::cout << gun1 << std::endl;
-    std::cout << gun2 << std::endl;
-
-    //Game game;
-    //game.run();
+    Game game;
+    game.run();
 
     return 0;
 }
