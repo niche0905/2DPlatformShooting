ead62165127f9ac72add68f134467cf9128e0c64
niche0905 <yudle537@gmail.com>
2024-09-01 17:20:43 +0900

    총기들 불러오기 테스트
    테스트 후 콘솔에 띄워 확인 해봄

--- 2D Platform Shooting/main.cpp
+++ 2D Platform Shooting/main.cpp
@@ -12,18 +12,10 @@ int main()//
   //-----// Game 실행하기 위한 Main
 {
     std::vector<Gun> guns;
-    guns.reserve(9);
-    guns.push_back(Gun("Pistol", 10.0f, 500.0f, -1, 450, -1));  // 기본 총으로 탄창이 무한하게
-    guns.push_back(Gun("AK-47", 20.0f, 600.0f, 60, 600, 1));
-    guns.push_back(Gun("M4A1", 15.0f, 640.0f, 75, 700, 1));
-    guns.push_back(Gun("MP5", 12.0f, 560.0f, 90, 900, 1));
-    guns.push_back(Gun("P90", 10.0f, 700.0f, 120, 1000, 1));
-    guns.push_back(Gun("Uzi", 15.0f, 800.0f, 90, 600, 1));
-    guns.push_back(Gun("M249", 17.0f, 750.0f, 200, 800, 1));
-    guns.push_back(Gun("Barrett-M82", 100.0f, 600.0f, 3, 60, 1));
-    guns.push_back(Gun("AWM", 80.0f, 800.0f, 5, 120, 1));
+    loadGunFromFile(GunSavePath, guns);
 
-    saveGunFromFile(GunSavePath, guns);
+    for (const Gun& gun : guns)
+        std::cout << gun << std::endl;
 
 
     Game game;
