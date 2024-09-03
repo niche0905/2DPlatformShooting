f1057da5d202f211e9d870ccfea843752e70b930
KyeongWonMin(laptop) <sojoonghan2@naver.com>
2024-09-03 15:17:24 +0900

    Game 전역으로 뺌

--- 2D Platform Shooting/main.cpp
+++ 2D Platform Shooting/main.cpp
@@ -1,12 +1,14 @@
 #include "Game.h"
 
+Game game;
+
       //-----//
      //     //  (っ´ω`)っ
     //-----//
 int main()//
   //-----// Game 실행하기 위한 Main
 {
-    Game game;
+    
     game.run();
 
     return 0;
