1f88c60862bf184646001bc991fbe18032eb3c7b
KyeongWonMin(laptop) <sojoonghan2@naver.com>
2024-11-05 10:02:39 +0900

    C_NM Init() 추가

---
+++ 2D Platform Shooting/ClientNetworkManager.cpp
@@ -0,0 +1,83 @@
+#include "pch.h"
+#include "ClientNetworkManager.h"
+
+ClientNetworkManager::ClientNetworkManager()
+{
+
+}
+
+ClientNetworkManager::~ClientNetworkManager()
+{
+
+}
+
+void ClientNetworkManager::Init()
+{
+    // 윈속 초기화
+    WSADATA wsa;
+    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) return;
+
+    // 소켓 생성
+    client_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (client_socket == INVALID_SOCKET) //err_quit("socket()");
+
+    // 이벤트 핸들 생성
+    recvEvent = CreateEvent(NULL, FALSE, FALSE, NULL);
+    if (recvEvent == NULL) {
+        closesocket(client_socket);
+        //err_quit("Failed to create receive event");
+    }
+
+    processEvent = CreateEvent(NULL, FALSE, FALSE, NULL);
+    if (processEvent == NULL) {
+        CloseHandle(recvEvent);
+        closesocket(client_socket);
+        //err_quit("Failed to create process event");
+    }
+
+    // 큐 초기화
+    while (!process_queue.empty()) {
+        process_queue.pop();
+    }
+
+    // 클라이언트 ID 초기화
+    ClientID = -1;
+
+    // 스레드 핸들 초기화
+    client_thread = NULL;
+}
+
+void ClientNetworkManager::Connect()
+{
+
+}
+
+void ClientNetworkManager::CreateRecvThread()
+{
+
+}
+
+void ClientNetworkManager::PushBuffer(char buf[MAX_SIZE])
+{
+
+}
+
+void ClientNetworkManager::SendPacket(char buf[MAX_SIZE])
+{
+
+}
+
+void ClientNetworkManager::PushBuffer()
+{
+
+}
+
+void ClientNetworkManager::Update()
+{
+
+}
+
+void ClientNetworkManager::Draw()
+{
+
+}
--- 2D Platform Shooting/ClientNetworkManager.h
+++ 2D Platform Shooting/ClientNetworkManager.h
@@ -3,21 +3,32 @@
 
 class ClientNetworkManager {
 private:
+	// 소켓
 	HANDLE socket;
+	// 스레드
 	HANDLE thread;
+	// 스레드 전달 큐
 	std::queue<std::array<char, MAX_SIZE>> process_queue;
+	// 클라이언트 ID
 	int ClientID;
+	// 이벤트
 	HANDLE recvEvent;
 	HANDLE processEvent;
 
 public:
-	ClientNetworkManager() {}
-	~ClientNetworkManager() {}
+	ClientNetworkManager();
+	~ClientNetworkManager();
 
+	void Init();
 	void Connect();
+
+	// Recv() 스레드 생성
 	void CreateRecvThread();
+	// enQueue
 	void PushBuffer(char buf[MAX_SIZE]);
+	// 패킷 전송
 	void SendPacket(char buf[MAX_SIZE]);
+
 	void PushBuffer();
 	void Update();
 	void Draw();
