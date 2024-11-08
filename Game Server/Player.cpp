e60a87906420c17bcdbc5555fd00efe73dd14bfb
niche0905 <yudle537@gmail.com>
2024-11-08 11:59:42 +0900

    Player 객체 선언과 생성자
    Player 객체의 기본 선언과 생성자를 완성하였다

--- Game Server/Player.cpp
+++ Game Server/Player.cpp
@@ -1,2 +1,12 @@
 #include "pch.h"
 #include "Player.h"
+
+
+Player::Player(bool dir) 
+	: directon(dir)
+	, life(BaseLife)
+	, gunId(BaseGunID)
+	, curMag(-1)
+{
+
+}
--- Game Server/Player.h
+++ Game Server/Player.h
@@ -1,8 +1,22 @@
 #pragma once
 #include "Object.h"
 
+constexpr int BaseLife = 3;
+constexpr uint8_t BaseGunID = 0;
+
+
 class Player : Object
 {
+private:
+	bool directon;
+	int life;
+	uint8_t gunId;
+	int curMag;
+
+public:
+	Player() = default;
+
+	Player(bool dir);
 
 };
 
