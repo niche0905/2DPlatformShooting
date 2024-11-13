8edb150c2cd6c0954d78e859371a2bcfe6656e79
niche0905 <yudle537@gmail.com>
2024-11-13 20:41:05 +0900

    World Update 구현
    World Update를 구현하였다

--- Game Server/World.cpp
+++ Game Server/World.cpp
@@ -10,7 +10,44 @@ World::World()
 
 void World::Update()
 {
+	// p1 p2는 위치값을 MOVE 패킷으로 받을 예정
 
+	for (Bullet& b : b1)
+		b.Update();
+	for (Bullet& b : b2)
+		b.Update();
+	for (Item& item : items)
+		item.Update();
+
+	// 충돌처리 구현
+
+	for (Bullet& b : b2) {
+		if (p1.Collision(b)) {
+			// 데미지 패킷 전송
+		}
+	}
+	for (Bullet& b : b1) {
+		if (p2.Collision(b)) {
+			// 데미지 패킷 전송
+		}
+	}
+
+	// 아이템 충돌처리의 순서 편파 판정 아님?
+	for (Item& item : items) {
+		bool p1_collision = p1.Collision(item);
+		bool p2_collision = p2.Collision(item);
+
+		if (p1_collision and p2_collision) {
+			// 이거 어떻게 함?
+		}
+		else if (p1_collision) {
+			// 총 업데이트 처리 및 패킷 보내기
+		}
+		else if (p2_collision) {
+			// 총 업데이트 처리 및 패킷 보내기
+		}
+		// 방어적인 코딩
+	}
 }
 
 void World::Process()
