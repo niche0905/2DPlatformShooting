2750ad4adb16e4e9d68c61bb5f26b84a48c12739
niche0905 <yudle537@gmail.com>
2024-11-14 12:34:19 +0900

    MovingObect 구현
    MovingObject에서 추가되는 것들을 구현하였다

---
+++ Game Server/MovingObject.cpp
@@ -0,0 +1,41 @@
+#include "pch.h"
+#include "MovingObject.h"
+
+
+MovingObject::MovingObject()
+	: Object()
+	, velX(0.0f)
+	, velY(0.0f)
+{
+
+}
+
+MovingObject::MovingObject(float pos_x, float pos_y, float width, float height, float pivot_x, float pivot_y)
+	: Object(pos_x, pos_y, width, height, pivot_x, pivot_y)
+	, velX(0.0f)
+	, velY(0.0f)
+{
+
+}
+
+void MovingObject::Update()
+{
+	// TODO : deltaTime 만큼 velocity 만큼 이동하여야 함
+}
+
+Velocity MovingObject::getVelocity() const
+{
+	return Velocity(velX, velY);
+}
+
+void MovingObject::setVelocity(Velocity velocity)
+{
+	velX = velocity.velX;
+	velY = velocity.velY;
+}
+
+void MovingObject::setVelocity(float vel_x, float vel_y)
+{
+	velX = vel_x;
+	velY = vel_y;
+}
