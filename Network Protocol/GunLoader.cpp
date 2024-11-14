79f2c008db0aed0f5dd80a0a7f84934e0a6b5024
niche0905 <yudle537@gmail.com>
2024-11-14 21:59:50 +0900

    GunLoader 생성
    모든 게임 관련 프로그램에서 Gun 정보를 통일 시켜 보아야 하기 때문에 라이브러리에 옮기는 작업을 하였다
    이제 GunLoader 를 불러서 gun_table에 접근하는 것이 좋을 것 같다

---
+++ Network Protocol/GunLoader.cpp
@@ -0,0 +1,54 @@
+#include "pch.h"
+#include "GunLoader.h"
+#include <random>
+#include <ranges>
+
+
+std::ostream& operator<<(std::ostream& os, const Gun& gun)
+{
+	os << "[ " << gun.name << " ]" << std::endl;
+	os << "Damage: " << gun.damage << std::endl;
+	os << "Speed: " << gun.speed << std::endl;
+	os << "Mag: " << gun.mag << std::endl;
+	os << "RPM: " << gun.RPM << std::endl;
+	os << "Ratio: " << gun.ratio << std::endl;
+
+	return os;
+}
+
+std::istream& operator>>(std::istream& is, Gun& gun)
+{
+	std::string temp;
+	is >> temp >> gun.name >> temp;
+	is >> temp >> gun.damage;
+	is >> temp >> gun.speed;
+	is >> temp >> gun.mag;
+	is >> temp >> gun.RPM;
+	is >> temp >> gun.ratio;
+
+	return is;
+}
+
+const int GunLoader::getRandomGunId()
+{
+	int total{};
+
+	// ratio를 모두 더한다.
+	std::ranges::for_each(gun_table | std::views::values, [&total](const auto& gun) {
+		total += gun.ratio > 0 ? gun.ratio : 0;
+		});
+
+	// 난수를 생성한다.
+	std::uniform_int_distribution dist(1, total);
+	int random_value{ dist(RANDOM_ENGINE) };
+
+	// ratio에 따라 값을 반환한다.
+	int cnt{};
+	for (const auto& gun : gun_table | std::views::values) {
+		random_value -= gun.ratio > 0 ? gun.ratio : 0;
+		if (random_value <= 0) { break; }
+		++cnt;
+	}
+
+	return cnt;
+}
