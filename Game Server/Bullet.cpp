fb5671db953c7ca935968a8bdc161b8b979ffa22
niche0905 <yudle537@gmail.com>
2024-11-14 21:44:45 +0900

    Bullet 상속 부모 변경
    Bullet의 부모를 Object 에서 MovingObject 로 변경하였다
    velocity가 추가되었는데 Bullet의 경우 필요했나 싶다

--- Game Server/Bullet.cpp
+++ Game Server/Bullet.cpp
@@ -3,19 +3,19 @@
 
 
 Bullet::Bullet(uint8_t bullet_type, bool dir)
-	: Object(0.0f, 0.0f, myNP::BulletWidth, myNP::BulletHeight, 0.5, 0.5)
+	: MovingObject(0.0f, 0.0f, myNP::BulletWidth, myNP::BulletHeight, 0.5, 0.5)
 	, b_type(bullet_type)
 	, direction(dir)
 {
-
+	//setVelocity(0.0f, 0.0f); b_type에 맞게 속도 설정
 }
 
 Bullet::Bullet(float x, float y, uint8_t bullet_type, bool dir)
-	: Object(x, y, myNP::BulletWidth, myNP::BulletHeight, 0.5, 0.5)
+	: MovingObject(x, y, myNP::BulletWidth, myNP::BulletHeight, 0.5, 0.5)
 	, b_type(bullet_type)
 	, direction(dir)
 {
-
+	//setVelocity(0.0f, 0.0f); b_type에 맞게 속도 설정
 }
 
 void Bullet::Update()
@@ -23,5 +23,5 @@ void Bullet::Update()
 	// TODO : deltaTime 만큼 이동
 	//		  여기서도 gun_id (b_type) 으로 사용
 
-
+	
 }
