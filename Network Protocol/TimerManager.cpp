3a716ac27fdf2e9d833c4e8e51d316c24ed5a6b3
niche0905 <yudle537@gmail.com>
2024-11-18 21:50:11 +0900

    TimerManager 선언
    TM을 선언만 하였다
    추가로 필요한 함수 생각해보아야 하고
    우리만의 epochTime을 생각해야 한다

---
+++ Network Protocol/TimerManager.cpp
@@ -0,0 +1,27 @@
+#include "pch.h"
+#include "TimerManager.h"
+
+void TimerManager::Init()
+{
+
+}
+
+void TimerManager::Update()
+{
+
+}
+
+bool TimerManager::isSyncTime()
+{
+	return false;
+}
+
+std::chrono::milliseconds TimerManager::epochToMillis()
+{
+	return std::chrono::milliseconds();
+}
+
+std::chrono::milliseconds TimerManager::timeGap(std::chrono::milliseconds bullet_fire_t)
+{
+	return std::chrono::milliseconds();
+}
---
+++ Network Protocol/TimerManager.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <chrono>
+
+
+class TimerManager
+{
+private:
+	std::chrono::system_clock::time_point oldTime;		// 이전 업데이트 프레임 시점
+	std::chrono::milliseconds deltaTime;				// deltatTime
+
+public:
+	void Init();		// 초기화
+	void Update();		// Update
+	bool isSyncTime();	// 동기화 시간인지 알려줌 (지금이 타이밍인지)
+	std::chrono::milliseconds epochToMillis();		// epoch 시간으로 부터의 차이 (우리만의 epoch)
+	std::chrono::milliseconds timeGap(std::chrono::milliseconds bullet_fire_t);		// 총알이 발사 되었을 때와 시차 (ping 차이를 보간하기 위한)
+
+};
+
