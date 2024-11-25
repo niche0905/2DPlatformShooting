0bcf6e5165ff49d1d834737c606bd102f85d2234
niche0905 <yudle537@gmail.com>
2024-11-25 21:02:20 +0900

    클라 queue 삭제

--- 2D Platform Shooting/ClientNetworkManager.h
+++ 2D Platform Shooting/ClientNetworkManager.h
@@ -3,6 +3,8 @@
 #include "Scene.h"
 #include "SceneManager.h"
 
+DWORD WINAPI WorkerRecv(LPVOID arg);
+
 class ClientNetworkManager {
 private:
 	// 소켓
@@ -10,7 +12,7 @@ private:
 	// 스레드
 	HANDLE clientThread;
 	// 스레드 전달 큐
-	std::queue<std::array<char, MAX_SIZE>> process_queue;
+	//std::queue<std::array<char, MAX_SIZE>> process_queue;
 	// 클라이언트 ID
 	uint32_t ClientID;
 	// 이벤트
@@ -31,7 +33,7 @@ public:
 	void CreateRecvThread();
 	// enQueue
 	// 서버에서 데이터가 넘어오면 Event 처리 스레드에게 할 일을 넘기는 함수
-	void PushBuffer(char buf[MAX_SIZE]);
+	//void PushBuffer(char buf[MAX_SIZE]);
 	// 패킷 전송
 	void SendPacket(char* buf, uint8_t packet_id);
 
