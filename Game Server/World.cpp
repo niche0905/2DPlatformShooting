9a436f34cfd9541f83129bdd826eea8306995732
niche0905 <yudle537@gmail.com>
2024-11-29 23:46:51 +0900

    서버 버그 수정
    아이템 소모 시 보내는 패킷을 잘못 설정한 것을 수정

--- Game Server/World.cpp
+++ Game Server/World.cpp
@@ -101,7 +101,7 @@ void World::CollisionCheck()
 
 			// 아이템이 사라졌으니 (서버에서 소모되었음) 클라이언트에서 Remove 해주어라 <- 반복되는 코드들 모듈화가 필요해 보임
 			SNMgr.SendPacket<myNP::SC_ITEM_REMOVE_PACKET>(static_cast<int32_t>(0), item_id);
-			SNMgr.SendPacket<myNP::SC_ITEM_CREATE_PACKET>(static_cast<int32_t>(1), item_id);
+			SNMgr.SendPacket<myNP::SC_ITEM_REMOVE_PACKET>(static_cast<int32_t>(1), item_id);
 		}
 		else if (p1_collision) {
 			p1.GunUpdate(GunInfo.getRandomGunId());
@@ -110,7 +110,7 @@ void World::CollisionCheck()
 			it = items.erase(it);
 
 			SNMgr.SendPacket<myNP::SC_ITEM_REMOVE_PACKET>(static_cast<int32_t>(0), item_id);
-			SNMgr.SendPacket<myNP::SC_ITEM_CREATE_PACKET>(static_cast<int32_t>(1), item_id);
+			SNMgr.SendPacket<myNP::SC_ITEM_REMOVE_PACKET>(static_cast<int32_t>(1), item_id);
 		}
 		else if (p2_collision) {
 			p2.GunUpdate(GunInfo.getRandomGunId());
@@ -119,7 +119,7 @@ void World::CollisionCheck()
 			it = items.erase(it);
 
 			SNMgr.SendPacket<myNP::SC_ITEM_REMOVE_PACKET>(static_cast<int32_t>(0), item_id);
-			SNMgr.SendPacket<myNP::SC_ITEM_CREATE_PACKET>(static_cast<int32_t>(1), item_id);
+			SNMgr.SendPacket<myNP::SC_ITEM_REMOVE_PACKET>(static_cast<int32_t>(1), item_id);
 		}
 		else {
 			++it;
