37b5a7fd8ac78b7fa76886425492ce827ea61938
niche0905 <yudle537@gmail.com>
2024-11-29 21:22:37 +0900

    ITEM REMOVE 패킷 전송
    아이템이 소모되어 클라이언트로 아이템을 지워라라는 패킷을 보내는 작업을 해주었다

--- Game Server/World.cpp
+++ Game Server/World.cpp
@@ -88,9 +88,6 @@ void World::CollisionCheck()
 			++it;
 	}
 
-	// TODO : 아이템을 먹고 랜덤으로 총기를 주어주는 로직을 가져오고
-	//		  Gun Update 전송 패킷을 구현하고 전송해야 함
-
 	for (auto it = items.begin(); it != items.end(); ) {
 		bool p1_collision = p1.Collision(*it);
 		bool p2_collision = p2.Collision(*it);
@@ -99,17 +96,30 @@ void World::CollisionCheck()
 			p1.GunUpdate(GunInfo.getRandomGunId());
 			p2.GunUpdate(GunInfo.getRandomGunId());
 
+			uint32_t item_id = (*it).GetItemID();
 			it = items.erase(it);
+
+			// 아이템이 사라졌으니 (서버에서 소모되었음) 클라이언트에서 Remove 해주어라 <- 반복되는 코드들 모듈화가 필요해 보임
+			SNMgr.SendPacket<myNP::SC_ITEM_REMOVE_PACKET>(static_cast<int32_t>(0), item_id);
+			SNMgr.SendPacket<myNP::SC_ITEM_CREATE_PACKET>(static_cast<int32_t>(1), item_id);
 		}
 		else if (p1_collision) {
 			p1.GunUpdate(GunInfo.getRandomGunId());
 
+			uint32_t item_id = (*it).GetItemID();
 			it = items.erase(it);
+
+			SNMgr.SendPacket<myNP::SC_ITEM_REMOVE_PACKET>(static_cast<int32_t>(0), item_id);
+			SNMgr.SendPacket<myNP::SC_ITEM_CREATE_PACKET>(static_cast<int32_t>(1), item_id);
 		}
 		else if (p2_collision) {
 			p2.GunUpdate(GunInfo.getRandomGunId());
 
+			uint32_t item_id = (*it).GetItemID();
 			it = items.erase(it);
+
+			SNMgr.SendPacket<myNP::SC_ITEM_REMOVE_PACKET>(static_cast<int32_t>(0), item_id);
+			SNMgr.SendPacket<myNP::SC_ITEM_CREATE_PACKET>(static_cast<int32_t>(1), item_id);
 		}
 		else {
 			++it;
