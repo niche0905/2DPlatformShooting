f1d60a667d69f0107e67a309c9363e573648dba0
niche0905 <yudle537@gmail.com>
2024-12-02 10:38:11 +0900

    서버 Bullet 부족한 함수 처리
    Bullet을 사용하기에 부족한 것들을 처리하였다

--- Game Server/Bullet.h
+++ Game Server/Bullet.h
@@ -5,16 +5,22 @@
 class Bullet : public MovingObject
 {
 private:
+	uint32_t b_id;
+
 	uint8_t b_type;
 	bool direction;
 
 public:
 	Bullet() = default;
 
-	Bullet(uint8_t bullet_type, bool dir);
-	Bullet(float x, float y, uint8_t bullet_type, bool dir);
+	Bullet(uint32_t bullet_id, uint8_t bullet_type, bool dir);
+	Bullet(float x, float y, uint32_t bullet_id, uint8_t bullet_type, bool dir);
 
 	void Update(int64_t delta_time);
+
+	uint32_t GetBulletID() const;
+	uint8_t GetBulletType() const;
+	bool GetDirection() const;
 
 };
 
